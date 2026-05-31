4fd30b1fdd16bce211c046e85254a16449bf358b
Z User <z@container>
2026-05-31 23:19:45 +0000

    Add maintenance overlay 01/06-03/06/2026

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* PWA Service Worker - KPI BVNT An Giang */
-const CACHE_NAME = 'kpi-bvnt-v10';
+const CACHE_NAME = 'kpi-bvnt-v11';
 const OFFLINE_URLS = ['/'];
 
 self.addEventListener('install', function(event) {
