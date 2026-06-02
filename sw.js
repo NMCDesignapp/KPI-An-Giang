61bf2a84ef955daf975b9775039bcb1a439c59a5
Z User <z@container>
2026-06-02 01:54:13 +0000

    Lighten background colors for Thi Dua/Chinh Sach/CLB pages

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* PWA Service Worker - KPI BVNT An Giang */
-const CACHE_NAME = 'kpi-bvnt-v13';
+const CACHE_NAME = 'kpi-bvnt-v14';
 const OFFLINE_URLS = ['/'];
 
 self.addEventListener('install', function(event) {
