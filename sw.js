513a9e82183fd3dc22905a6b3c003a11d596dc62
Z User <z@container>
2026-06-02 04:36:13 +0000

    Remove maintenance overlay

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* PWA Service Worker - KPI BVNT An Giang */
-const CACHE_NAME = 'kpi-bvnt-v14';
+const CACHE_NAME = 'kpi-bvnt-v15';
 const OFFLINE_URLS = ['/'];
 
 self.addEventListener('install', function(event) {
