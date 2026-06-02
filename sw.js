fd35994f7de448484e668e971551c631fe7f9163
Z User <z@container>
2026-06-02 08:15:52 +0000

    Sync JS rank colors with CSS for CLB/ThiDua pages (Vang=gold, BachKim=silver, KimCuong=teal)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* PWA Service Worker - KPI BVNT An Giang */
-const CACHE_NAME = 'kpi-bvnt-v16';
+const CACHE_NAME = 'kpi-bvnt-v17';
 const OFFLINE_URLS = ['/'];
 
 self.addEventListener('install', function(event) {
